4bf5fd3dafd4d205a9d11c9856a0eaef1e2b6ce5
yxzlwz <danny070601@aliyun.com>
2023-06-07 21:18:31 +0800

    [update] page check before yiyan request in js

--- .vuepress/public/js/main.js
+++ .vuepress/public/js/main.js
@@ -1,16 +1,20 @@
-fetch('https://api.yixiangzhilv.com/yiyan/sentence/')
-  .then(res => res.json())
-  .then(res => {
-    if (res.provenance && res.author) {
-      res.from_show = `${res.provenance} · ${res.author}`;
-    } else {
-      res.from_show = `${res.provenance || res.author}`;
-    }
-    const tagline = document.querySelector(
-      '#app > div > div > div > div.home-wrapper > section.banner-brand__wrapper > div > p.tagline'
-    );
-    tagline.innerHTML = `${res.content} - 「${res.from_show}」`;
-  })
-  .catch(error => {
-    console.log('请求发生错误:', error);
-  });
+if (location.pathname === '/') {
+  fetch('https://api.yixiangzhilv.com/yiyan/sentence/')
+    .then(res => res.json())
+    .then(res => {
+      if (res.provenance && res.author) {
+        res.from_show = `${res.provenance} · ${res.author}`;
+      } else {
+        res.from_show = `${res.provenance || res.author}`;
+      }
+      const tagline = document.querySelector(
+        '#app > div > div > div > div.home-wrapper > section.banner-brand__wrapper > div > p.tagline'
+      );
+      if (tagline) {
+        tagline.innerHTML = `${res.content} - 「${res.from_show}」`;
+      }
+    })
+    .catch(error => {
+      console.log('请求发生错误:', error);
+    });
+}
